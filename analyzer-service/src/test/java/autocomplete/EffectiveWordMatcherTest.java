241a27fc85152ea6ddf0046e815ed663102780a9
bing.a.qian <qianyitiandedie@qq.com>
2020-12-18 11:28:35 +0800

    favorite symbols change format

---
+++ analyzer-service/src/test/java/autocomplete/EffectiveWordMatcherTest.java
@@ -0,0 +1,12 @@
+package autocomplete;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EffectiveWordMatcherTest {
+
+    @Test
+    void dynamicAddNew() {
+    }
+}
---
+++ analyzer-service/src/test/java/autocomplete/SimpleWordMatcherTest.java
@@ -0,0 +1,25 @@
+package autocomplete;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleWordMatcherTest {
+
+    @Test
+    void buildPrefixMatchers() {
+    }
+
+    @Test
+    void dynamicAddNew() {
+
+//        String inputText = req.getParameter("inputText");
+//        Set<String> matchers = wordMatcher.obtainMatchedWords(inputText);
+//        StringBuilder sb = new StringBuilder();
+//        for (String m: matchers) {
+//            sb.append(m);
+//            sb.append(' ');
+//        }
+//        sb.deleteCharAt(sb.length()-1);
+    }
+}
