65844a796885c1c374e9ec5e3f3ecaa89847fa13
bing.a.qian <qianyitiandedie@qq.com>
2021-01-10 22:26:53 +0800

    home page

---
+++ analyzer-service/src/main/java/com/qianyitian/hope2/analyzer/config/MyMvcConfig.java
@@ -0,0 +1,21 @@
+package com.qianyitian.hope2.analyzer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 拦截某个请求跳转固定位置
+     *
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //super.addViewControllers(registry);
+        registry.addViewController("/").setViewName("welcome");
+        registry.addViewController("/menu.json").setViewName("menu.json");
+    }
+}
